32388993cc614aa5f4229379138e02c298f873dd
WagnerCSN <wagcostvez@gmail.com>
2023-05-20 23:45:55 -0300

    add page profile

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,39 +1,41 @@
 import  styled  from "styled-components" 
 export const Container = styled.div`
    width: 100%;
-    
+   height: 100vh;
     >header{ 
         width: 100%;
         height: 144px;
         background-color: ${({theme}) => theme.COLORS.BACKGROUND_400};
         padding: 64px 124px;
 
-;
-    a{
-        height: 144px;
-        width: 100%;
-        color:${({theme}) => theme.COLORS.PINK};
-        font-size: 16px;
-        
-        
+        a{
+            height: 144px;
+            width: 100%;
+            color:${({theme}) => theme.COLORS.PINK};
+            font-size: 16px;
+        }
     }
-}
 
     >form{
         max-width: 340px;
-        margin: 0 auto;
-        padding: 64px 0 59px 144px;
+        margin: -85px auto;
+    
+        > div:nth-child(3){
+            margin-bottom: 24px;
+        }
     }
 `;
 
 export const Avatar = styled.div`
+     width: 186px;
+    height: 186px;
     position: relative;
-    margin: -160px auto 64px;
+    margin: 0 auto 64px;
+
     >img{ 
         width: 186px;
         height: 186px;
         border-radius: 50%;
-
     }
 
     >label{
@@ -52,5 +54,4 @@ export const Avatar = styled.div`
             display: none;
         }
     }
-
 `;
